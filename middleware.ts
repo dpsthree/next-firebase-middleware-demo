cab11dfdcbb1f365b80443ca22eb7cb86d490c0b
Paul Spears <dpsthree@gmail.com>
2022-09-27 14:07:59 -0500

    capture todo list

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,14 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
+// Must keep this simple as it is not Node based:
+// https://nextjs.org/docs/api-reference/edge-runtime
 export function middleware(request: NextRequest) {
+  // Find out if the user exists
+  // If not, create them
+  // Now that there is a user determine if they have access to the system
+  // If they don't, redirect them to the subscription page
+  // If so, continue
   return NextResponse.redirect(new URL('/login', request.url))
 }
 
