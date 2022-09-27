746f8a0ac83a5cb3a471b50c398d8b3d7c89048d
Paul Spears <dpsthree@gmail.com>
2022-09-27 14:32:00 -0500

    call function with no security

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,29 @@
 // middleware.ts
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 // Must keep this simple as it is not Node based:
 // https://nextjs.org/docs/api-reference/edge-runtime
-export function middleware(request: NextRequest) {
+
+const helloWorldURL =
+  'http://localhost:5001/next-middleware-demo/us-central1/helloWorld';
+
+export async function middleware(request: NextRequest) {
+  try {
+    const results = await (await fetch(helloWorldURL)).json();
+    console.log(results);
+  } catch (e) {
+    console.log(e);
+  }
   // Find out if the user exists
   // If not, create them
   // Now that there is a user determine if they have access to the system
   // If they don't, redirect them to the subscription page
   // If so, continue
-  return NextResponse.redirect(new URL('/login', request.url))
+  return NextResponse.redirect(new URL('/login', request.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: '/',
-}
+};
