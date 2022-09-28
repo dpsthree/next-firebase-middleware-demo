f24115a9e8737a3b8e017be57d332058f98a603c
Paul Spears <dpsthree@gmail.com>
2022-09-28 15:34:22 -0500

    repro

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,13 @@
-import * as functions from 'firebase-functions';
 import { onRequest } from 'firebase-functions/v2/https';
-import * as admin from 'firebase-admin';
-
-import { connectFirebaseAdminTools } from './firebase-admin-tools';
-import { User } from './models/user';
 
 const app = require('next')({
   dir: '../',
+  // Build the next app and 
+  // toggle the following to false
+  // and the hosting works fine
+  dev: true,
+  // Update the following if your emulator environment
+  // differs for the hosting port
   port: 5000,
   hostname: 'localhost',
 });
@@ -18,40 +19,7 @@ export const server = onRequest((request, response) => {
     request.originalUrl
   );
   return prepared.then(() => {
-    console.log('app is prepared')
+    console.log('app is prepared');
     return handle(request, response);
   });
 });
-
-const USER_PATH = 'users';
-export const helloWorld = functions.https.onRequest(
-  async (request, response) => {
-    connectFirebaseAdminTools();
-    const token = request.query['token'];
-    if (typeof token !== 'string') {
-      response.status(400).send(`Invalid token: ${token}`);
-    } else {
-      const user = await createUserIfNotExists(token);
-      response.send(user);
-    }
-  }
-);
-
-async function createUserIfNotExists(user: string): Promise<User> {
-  try {
-    const userDoc = await admin.firestore().doc(`/${USER_PATH}/${user}`).get();
-    if (userDoc.exists) {
-      return userDoc.data() as User;
-    } else {
-      const newUser: User = {
-        id: user,
-        canAccess: user === '12345',
-      };
-      await admin.firestore().doc(`/${USER_PATH}/${newUser.id}`).set(newUser);
-      return newUser;
-    }
-  } catch (e) {
-    console.log('failed to communicate with firestore', e);
-    throw e;
-  }
-}
--- functions/src/models/user.ts
+++
@@ -1,5 +0,0 @@
-export interface User {
-  // TODO: more fields coming in the future
-  id: string;
-  canAccess: boolean;
-}
